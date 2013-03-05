0fb66063ea11dc5aecb985c90a556a06dd99e52d
Edward KIM <fharenheit@gmail.com>
2013-03-05 16:34:18 +0900

    Ant Run 추가.

--- src/main/java/com/yourcompany/hadoop/mapreduce/Hdfs.java
+++ src/main/java/com/yourcompany/hadoop/mapreduce/Hdfs.java
@@ -1,16 +1,15 @@
 package com.yourcompany.hadoop.mapreduce;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
 
 import java.io.IOException;
 
 public class Hdfs {
 
     public static void main(String[] args) throws IOException {
-        Hdfs.move(args[0], args[1]);
+        Hdfs.copy(args[0], args[1]);
     }
 
     public static void printFileInfo(String path) throws IOException {
@@ -93,6 +92,37 @@ public class Hdfs {
             System.exit(0);
         }
         System.out.println("파일을 이동하지 못했습니다.");
+        System.exit(-1);
+    }
+
+    public static void copy(String source, String target) throws IOException {
+        Configuration conf = new Configuration();
+        conf.set("fs.default.name", "hdfs://125.141.144.168:9000");
+        FileSystem fs = FileSystem.get(conf);
+
+        if (!fs.exists(new Path(source))) {
+            System.err.println("지정한 경로의 디렉토리 또는 파일이 존재하지 않습니다.");
+            System.exit(-1);
+        }
+
+        FileStatus sourcePath = fs.getFileStatus(new Path(source));
+        if(!sourcePath.isDir()) {
+            if(fs.exists(new Path(target))) {
+                System.err.println("이미 파일이 있어서 복사할 수 없습니다.");
+                System.exit(-1);
+            }
+
+            FSDataInputStream in = fs.open(new Path(source));
+            FSDataOutputStream out = fs.create(new Path(target));
+            IOUtils.copyBytes(in, out, 4096);
+            IOUtils.closeStream(out);
+            IOUtils.closeStream(in);
+
+            System.out.println("파일을 복사했습니다.");
+            System.exit(0);
+        }
+
+        System.err.println("디렉토리는 복사할 수 없습니다.");
         System.exit(-1);
     }
 }
